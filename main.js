eb209fddc0c6e69c50d01f8b53acaeb9d0982f08
LN <leonelngoya@gmail.com>
2023-12-28 08:26:58 +0100

    first version

--- main.js
+++ main.js
@@ -4,91 +4,172 @@ gsap.registerPlugin(MotionPathPlugin);
 
 const nextBtn = document.querySelector(".navigation .next");
 const prevBtn = document.querySelector(".navigation .prev");
-const imagesContainer = document.querySelector(".images");
-const images = document.querySelectorAll(".images img");
+const images = document.querySelector(".images");
+const imagesContainer = document.querySelector(".images-container");
+const allImages = document.querySelectorAll(".images img");
 
 const img1 = document.querySelector(".image-1");
 const img2 = document.querySelector(".image-2");
 const img3 = document.querySelector(".image-3");
 
-const color1 = document.querySelector(".color-1");
-const color2 = document.querySelector(".color-2");
-const color3 = document.querySelector(".color-3");
-
-/*
-img1.style = `
-  width: 200px;
-  top: -10px;
-  left: -150px;
-  transform: rotate(28deg);
-`;
-img2.style = `
-  width: 700px;
-  top: 50%;
-  transform: translateY(calc(-50% + 50px));
-`;
-img3.style = `
-  width: 200px;
-  bottom: -70px;
-  left: -150px;
-  transform: rotate(120deg);
-`;
-*/
+let current = 1;
 
 nextBtn.addEventListener("click", () => {
-  if (img2.classList.contains("active")) {
-    const centerX = 150;
-    const centerY = 20;
-    const radius = 50;
-
-    gsap.to(img1, {
-      width: "700px",
-      rotate: 0,
-      motionPath: {
-        path: [
-          { x: centerX, y: centerY - radius },
-          { x: centerX, y: centerY - radius },
-        ],
-        align: img1,
-        ease: "linear",
-      },
-      duration: 2,
-    });
-
-    gsap.to(img2, {
-      width: "200px",
-      rotate: 0,
-      motionPath: {
-        path: [
-          { x: centerX, y: centerY - radius },
-          { x: centerX, y: centerY - radius },
-        ],
-        align: img1,
-        ease: "linear",
-      },
-      duration: 2,
-    });
-
-    gsap.to(img3, {
-      width: "200px",
-      rotate: 0,
-      motionPath: {
-        path: [
-          { x: centerX, y: centerY - radius },
-          { x: centerX, y: centerY - radius },
-        ],
-        align: img1,
-        ease: "linear",
-      },
-      duration: 2,
-    });
+  if (current < 4) {
+    current++;
+    switch (current) {
+      case 1:
+        break;
+      case 2:
+        gsap.to(img1, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img2, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img3, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+      case 3:
+        gsap.to(img3, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img1, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img2, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+      default:
+        gsap.to(img2, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img3, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img1, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+    }
   }
 });
 
 prevBtn.addEventListener("click", () => {
-  imagesContainer.style = `
-  
-  `;
+  if (current > 1) {
+    current--;
+    switch (current) {
+      case 1:
+        gsap.to(img2, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img3, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img1, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+      case 2:
+        gsap.to(img1, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img2, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img3, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+      case 3:
+        gsap.to(img3, {
+          width: "700px",
+          top: "-20px",
+          left: "100px",
+          transform: "rotate(0deg)",
+          //duration: .75,
+        });
+        gsap.to(img1, {
+          width: "250px",
+          top: "500px",
+          left: "-80px",
+          transform: "rotate(120deg)",
+          //duration: .75,
+        });
+        gsap.to(img2, {
+          width: "250px",
+          top: "-100px",
+          left: "-80px",
+          transform: "rotate(28deg)",
+          //duration: .75,
+        });
+        break;
+      default:
+        break;
+    }
+  }
 });
 
 //
